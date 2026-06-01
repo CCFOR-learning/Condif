8a31cbab75ed0d8f9d74dda932119f8bd1ff76fd
CCFOR-learning <153882667+CCFOR-learning@users.noreply.github.com>
2026-06-01 21:26:02 +0800

    Delete pipelines directory

--- pipelines/stable_diffusion/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pipeline_output import StableDiffusionPipelineOutput
-from .safety_checker import StableDiffusionSafetyChecker
-
-__all__ = ["StableDiffusionPipelineOutput", "StableDiffusionSafetyChecker"]
